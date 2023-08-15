80e4a26799ee459ef5e72afaf10043159c38f895
VENKATESH KODE <88055001+KodeVenkatesh@users.noreply.github.com>
2023-08-15 12:29:45 +0530

    Create count_digits_in_number.cpp

---
+++ 1. Mathematics/count_digits_in_number.cpp
@@ -0,0 +1,13 @@
+/***************************** 1. Count zeroes in a number *****************************8/
+//1. Optimal Approach
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n;
+    cin>>n;
+    cout<<floor(log10(n)+1);
+    return 0;
+}
+//Time Complexity:   O(1)
+//Space Complexity:  O(1)
